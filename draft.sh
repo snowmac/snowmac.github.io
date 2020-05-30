e21c0504ce54b96481115a5a1a6a92230619e6fc
Adam Bourg <adambourg@Adams-MBP.hsd1.co.comcast.net>
2020-05-29 21:56:23 -0600

    adding a script for easy authoring

---
+++ draft.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+timestamp=$(date +'%Y-%m-%d')
+argumentsAsString="$*"
+hypened="${argumentsAsString// /-}"
+filename="_drafts/$timestamp-$hypened.md"
+
+touch $filename
+
+echo "---" >> $filename
+echo "layout: post" >> $filename
+echo "title: \"$*\"" >> $filename
+echo "date: $(date +'%Y-%m-%d')" >> $filename
+echo "categories: " >> $filename
+echo "---" >> $filename
+echo "" >> $filename
+#subl $filename
