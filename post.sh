e51012b570f561e9d2df12bb2cf44afba7bd10bf
Adam Bourg <adam.bourg@gmail.com>
2022-08-09 01:12:24 -0600

    adding a helpful message to the post command

--- post.sh
+++ post.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 timestamp=$(date +'%Y-%m-%d')
+
+if [[ $# -eq 0 ]] ; then
+    echo "Please provide a title of the post"
+    echo "for example run the command like this:"
+    echo "./posh.sh I wrote a fancy bash program"
+    echo "The file name then becomes: _posts/$timestamp-I-wrote-a-fancy-bash-program.md"
+    exit 1
+fi
+
+
 argumentsAsString="$*"
 hypened="${argumentsAsString// /-}"
 filename="_posts/$timestamp-$hypened.md"
